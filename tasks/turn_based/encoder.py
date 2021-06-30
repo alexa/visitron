31f9a3058bf4186573df1b4ee79a984a54b21634
Ayush Shrivastava <shrivastava.ayush1996@gmail.com>
2021-06-30 18:00:22 -0400

    fixed a missed oscar import

--- tasks/turn_based/encoder.py
+++ tasks/turn_based/encoder.py
@@ -9,7 +9,7 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 
-from oscar.oscar import CaptionBertEncoder
+from oscar.modeling_bert import CaptionBertEncoder
 from transformers.pytorch_transformers.modeling_bert import (
     BertEmbeddings,
     BertLayerNorm,
